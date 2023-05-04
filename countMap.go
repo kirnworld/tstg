456998b893c9106fb93a0107c5ac55f805baaadc
kirnworld <dshunyakov@inbox.ru>
2023-05-04 15:36:37 +0300

    7

--- countMap.go
+++ countMap.go
@@ -1,1 +1,27 @@
 package main
+
+import (
+	"datafile"
+	"fmt"
+	"log"
+	"sort"
+)
+
+func main() {
+	var names []string
+	lines, err := datafile.GetStrings("names.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	counts := make(map[string]int)
+	for _, line := range lines {
+		counts[line]++
+	}
+	for name_v := range counts {
+		names = append(names, name_v)
+	}
+	sort.Strings(names)
+	for _, name := range names {
+		fmt.Printf("Votes for %s: %d\n", name, counts[name])
+	}
+}
